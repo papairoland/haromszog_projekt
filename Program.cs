630b25eea1d72f16a83d5f49c070de640b7d894e
PPetra08 <159010793+PPetra08@users.noreply.github.com>
2025-12-10 21:14:10 +0100

    Saját feladataim kész

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿
+
 namespace ConsoleApp18
 {
     internal class Program
@@ -6,23 +6,87 @@ namespace ConsoleApp18
         static void Main(string[] args)
         {
             UdvozloKep();
-            Pitagorasz();
-            Terulet();
             List<string> menupontok = new List<string>();
             menupontok.Add("Kerület számítása");
             menupontok.Add("Terület számítása");
+            menupontok.Add("Pitagorasz tétel");
+            menupontok.Add("Általános háromszög");
             menupontok.Add("Kilépés");
             ListazMenu(menupontok);
             Console.WriteLine("Vége!");
         }
 
+
+
+        // todo Petra feladata
         private static void ListazMenu(List<string> menupontok)
         {
-            //Petra feladata
-            Console.WriteLine("ide jött!");
-            //throw new NotImplementedException("Még nincs kész, ezért ne használd!");
-            Console.WriteLine("ide már jut el!");
+            Console.WriteLine("Válassz a menüpontok közül!");
+            for (int i = 0; i < menupontok.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {menupontok[i]}");
+            }
+
+            int MegadottValasz = 0;
+            bool Szam = false;
+
+
+            do
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+
+                if (key.Key == ConsoleKey.D1)
+                {
+                    MegadottValasz = 0;
+                    Szam = true;
+                }
+                else if (key.Key == ConsoleKey.D2)
+                {
+                    MegadottValasz = 1;
+                    Szam = true;
+                }
+                else if (key.Key == ConsoleKey.D3)
+                {
+                    MegadottValasz = 2;
+                    Szam = true;
+                }
+                else if (key.Key == ConsoleKey.D4)
+                {
+                    MegadottValasz = 3;
+                    Szam = true;
+                }
+                else if (key.Key == ConsoleKey.D5)
+                {
+                    MegadottValasz = 4;
+                    Szam = true;
+                }
+            }
+            while (!Szam);
+
+
+            switch (menupontok[MegadottValasz])
+            {
+                case "Kerület számítása":
+                    Kerulet();
+                    break;
+                case "Terület számítása":
+                    Terulet();
+                    break;
+                case "Pitagorasz tétel":
+                    Pitagorasz();
+                    break;
+                case "Általános háromszög":
+                    AltalanosHaromszog();
+                    break;
+                case "Kilépés":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    break;
+            }
         }
+
+
 
         private static void UdvozloKep()
         {
@@ -45,9 +109,42 @@ namespace ConsoleApp18
         private static void Terulet()
         {
             Console.WriteLine("Add meg a háromszög alapjához tartozó magasságát! ");
-            double magassag =Convert.ToDouble(Console.ReadLine());
+            double magassag = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Add meg a háromszög alapját! ");
-            double alap=Convert.ToDouble(Console.ReadLine());
+            double alap = Convert.ToDouble(Console.ReadLine());
+        }
+
+        // todo Petra feladata
+        private static void Kerulet()
+        {
+            Console.WriteLine("Add meg az első oldalt! ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Add meg a második oldalt! ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Add meg a harmadik oldalt! ");
+            double c = Convert.ToDouble(Console.ReadLine());
+            double kerulet = a + b + c;
+            Console.WriteLine($"A háromszög kerülete: {kerulet}");
+        }
+
+        // todo Petra feladata
+        private static void AltalanosHaromszog()
+        {
+            Console.WriteLine("Add meg az első oldalt! ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Add meg a második oldalt! ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Add meg a harmadik oldalt! ");
+            double c = Convert.ToDouble(Console.ReadLine());
+            if (a != b || a != c || b != c)
+            {
+                Console.WriteLine("A háromszög általános háromszög");
+            }
+            else
+            {
+                Console.WriteLine("A háromszög nem általános háromszög");
+            }
+
         }
     }
 }
